165de4fce9dbb977772e6bc618294f613cc3e985
Snickser <snickser@gmail.com>
2025-03-16 19:55:40 +0300

    tune

--- lang/en/enrol_yafee.php
+++ lang/en/enrol_yafee.php
@@ -33,8 +33,8 @@ $string['donate'] = '<div>Plugin version: {$a->release} ({$a->versiondisk})<br>
 You can find new versions of the plugin at <a href=https://github.com/Snickser/moodle-enrol_yafee>GitHub.com</a>
 <img src="https://img.shields.io/github/v/release/Snickser/moodle-enrol_yafee.svg"><br>
 Please send me some <a href="https://yoomoney.ru/fundraise/143H2JO3LLE.240720">donate</a>😊</div>
-TRX TRGMc3b63Lus6ehLasbbHxsb2rHky5LbPe<br>
 BTC 1GFTTPCgRTC8yYL1gU7wBZRfhRNRBdLZsq<br>
+TRX TRGMc3b63Lus6ehLasbbHxsb2rHky5LbPe<br>
 ETH 0x1bce7aadef39d328d262569e6194febe597cb2c9<br>
 <iframe src="https://yoomoney.ru/quickpay/fundraise/button?billNumber=143H2JO3LLE.240720"
 width="330" height="50" frameborder="0" allowtransparency="true" scrolling="no"></iframe><br>';
