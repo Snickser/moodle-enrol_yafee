89e9a356bb368955aa7a2904694d6c6463979e74
Snickser <snickser@gmail.com>
2025-03-27 22:30:16 +0300

    add

---
+++ db/install.php
@@ -0,0 +1,39 @@
+<?php
+// This file is part of the bank paymnts module for Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * enrol_yafee installer script.
+ *
+ * @package    enrol_yafee
+ * @copyright  2024 Alex Orlov <snickser@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+/**
+ * Enabler
+ *
+ * @return boolean
+ */
+function xmldb_enrol_yafee_install() {
+
+#    require_once('../config.php');
+#    require_once($CFG->libdir.'/adminlib.php');
+
+    $class = \core_plugin_manager::resolve_plugininfo_class('enrol');
+    $class::enable_plugin('yafee', true);
+
+    return true;
+}
