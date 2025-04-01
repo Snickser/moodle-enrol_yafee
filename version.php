1d1505b21b0b8bd424dbfe19f707ab37638b24e1
Snickser <snickser@gmail.com>
2025-04-01 12:09:00 +0300

    version

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2025032802;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2025040100;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2023100400;        // Requires this Moodle version.
 $plugin->component = 'enrol_yafee';     // Full name of the plugin (used for diagnostics).
-$plugin->release   = '1.4';
+$plugin->release   = '2.0';
 $plugin->maturity  = MATURITY_STABLE;
